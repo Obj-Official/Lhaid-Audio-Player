477abbb5409d9251893b1fb30cf5eb97d65aa243
David Ezeani <davidezeani11@gmal.com>
2024-12-23 00:51:46 +0100

    add MJ music

--- src/data/Tracks.js
+++ src/data/Tracks.js
@@ -2,10 +2,12 @@
 import Headlines from './Drake-Headlines.mp3';
 import Owa from './Falz-Owa-feat-Tekno.mp3';
 import ForMyHand from './Burna-Boy-For-My-Hand-feat-Ed-Sheeran.mp3';
+import TheWayYouMakeMeFeel from './SpotifyMate.com - The Way You Make Me Feel - Michael Jackson.mp3';
 // audio thumbnails
 import trinix from './DrakeHeadlines.jpg';
 import trinix2 from './Falz-Ft-Tekno-Owa-Artwork.jpg';
 import trinix3 from './burna-boy-ed-sheeran.jpg';
+import trinix4 from './SpotifyMate.com - The Way You Make Me Feel - Michael Jackson.jpg';
 
 export const tracks = [
 {
@@ -26,4 +28,16 @@ thumbnail: trinix,
     author: 'Burna Boy',
     thumbnail: trinix3,
     },
+  {
+    title: 'For My Hand Ft. Ed Sheeran',
+    src: ForMyHand,
+    author: 'Burna Boy',
+    thumbnail: trinix3,
+    },
+    {
+      title: 'The way you make me feel',
+      src: TheWayYouMakeMeFeel,
+      author: 'Micheal Jackson',
+      thumbnail: trinix4,
+      },
 ]
